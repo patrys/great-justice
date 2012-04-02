b31e2093ddad08c4d41bc358ff1fc9ba0b9709f8
Patryk Zawadzki <patrys@room-303.com>
2012-04-02 12:59:28 +0200

    Add some color where applicable

--- great_justice/__init__.py
+++ great_justice/__init__.py
@@ -3,11 +3,14 @@
 import contextlib
 import linecache
 import logging
+import os
 import re
 import sys
 
 VERSION = (2011, 9, 0, 'final', 0)
 IDENTIFIER_RE = re.compile('\W')
+
+logger = logging.getLogger('CATS')
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
@@ -20,10 +23,83 @@ def get_version():
             version = '%s %s %s' % (version, VERSION[3], VERSION[4])
     return version
 
-LOGGER = logging.getLogger('CATS')
+
+class Structure(object):
+
+    start_tag = ''
+    end_tag = '\033[0m'
+
+    def __init__(self, *args):
+        self.args = args
+
+    def __str__(self):
+        inner = ''.join(str(arg) for arg in self.args)
+        if self.is_interactive():
+            return '%s%s%s' % (self.start_tag, inner, self.end_tag)
+        return inner
+
+    def is_interactive(self):
+        return all(os.isatty(handler.stream.fileno())
+                   if hasattr(handler, 'stream') else False
+                   for handler in logger.handlers)
+
+
+class VariableName(Structure):
+
+    start_tag = '\033[95m'
+
+    def __init__(self, variable_name):
+        self.args = [variable_name]
+
+
+class VariableValue(Structure):
+
+    start_tag = '\033[95m'
+
+    def __init__(self, variable_value):
+        self.args = [variable_value]
+
+
+class Variable(Structure):
+
+    def __init__(self, variable_name, variable_value):
+        self.args = ['      ', VariableName(variable_name), ' = ',
+                     VariableValue(variable_value)]
+
+
+class CodeFileName(Structure):
+
+    start_tag = '\033[92m'
+
+    def __init__(self, filename):
+        self.args = [filename]
+
+
+class CodeLineNo(Structure):
+
+    start_tag = '\033[92m'
+
+    def __init__(self, line_no):
+        self.args = [line_no]
+
+
+class CodeObjectName(Structure):
+
+    start_tag = '\033[92m'
+
+    def __init__(self, object_name):
+        self.args = [object_name]
+
+
+class CodeLine(Structure):
+
+    def __init__(self, filename, line_no, object_name):
+        self.args = ['  File "', CodeFileName(filename), '", line ',
+                     CodeLineNo(line_no), ', in ', CodeObjectName(object_name)]
+
 
 def what_happen():
-    LOGGER.debug(u'一体どうしたと言んだ！')
+    logger.debug(u'一体どうしたと言んだ！')
     exc_type, exc_value, trace = sys.exc_info()
     while trace:
         frame = trace.tb_frame
@@ -34,22 +110,22 @@ def what_happen():
         local_base = __file__.rsplit('.', 1)[0]
         if filename_base == local_base:
             continue
-        LOGGER.debug(u'  File "%s", line %s, in %s' % (filename,
-                                                      frame.f_lineno,
-                                                      frame.f_code.co_name))
+        struct = CodeLine(filename, frame.f_lineno, frame.f_code.co_name)
+        logger.debug(struct)
         linecache.checkcache(filename)
         line = linecache.getline(filename, frame.f_lineno, frame.f_globals)
         if line:
             line = line.strip()
-            LOGGER.debug(u'    %s' % (line, ))
+            logger.debug(u'    %s' % (line, ))
             tokens = IDENTIFIER_RE.split(line)
             for key, value in frame.f_locals.items():
                 if key in tokens:
                     try:
-                        LOGGER.debug(u'      %s = %s' % (key, repr(value)))
-                    except:
-                        LOGGER.debug(u'      %s = <EXCEPTION RAISED WHILE TRYING TO PRINT>' % key)
-    LOGGER.debug(u'      %s' % (exc_value, ))
+                        structure = Variable(key, repr(value))
+                    except Exception:
+                        structure = Variable(key, '<EXCEPTION RAISED WHILE TRYING TO PRINT>')
+                    logger.debug(structure)
+    logger.debug(u'      %s' % (exc_value, ))
 
 @contextlib.contextmanager
 def take_your_time():
